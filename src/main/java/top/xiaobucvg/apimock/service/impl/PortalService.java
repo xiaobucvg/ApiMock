5b6a21267221c972ac3dfcba76611b58253f72df
xiaobu <www.1714005186@gmail.com>
2019-09-05 17:27:03 +0800

    新版本使用map重写了缓存，添加了错误页，修改了大量代码

--- src/main/java/top/xiaobucvg/apimock/service/impl/PortalService.java
+++ src/main/java/top/xiaobucvg/apimock/service/impl/PortalService.java
@@ -1,7 +1,7 @@
 package top.xiaobucvg.apimock.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.servlet.mvc.condition.PatternsRequestCondition;
 import org.springframework.web.servlet.mvc.condition.ProducesRequestCondition;
 import org.springframework.web.servlet.mvc.condition.RequestMethodsRequestCondition;
@@ -11,11 +11,14 @@ import top.xiaobucvg.apimock.controller.ApiController;
 import top.xiaobucvg.apimock.pojo.Api;
 import top.xiaobucvg.apimock.pojo.Response;
 import top.xiaobucvg.apimock.service.IPortalService;
-import top.xiaobucvg.apimock.util.ApiCache;
-import top.xiaobucvg.apimock.util.ResponseCreater;
+import top.xiaobucvg.apimock.util.ApiCache_v1_5;
+import top.xiaobucvg.apimock.util.ResponseCreator;
+import top.xiaobucvg.apimock.util.WebApplicationAware;
 
 import java.lang.reflect.Method;
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
 
 /***
  *
@@ -25,84 +28,121 @@ import java.util.List;
  */
 @Service
 public class PortalService implements IPortalService {
-
+    @Autowired
+    private WebApplicationAware webApplicationAware;
 
     /***
-     * 注册api
+     * 1.5版本新增的注册Api的方法
+     *
+     * @since v1.5
+     *
+     * by Mr.Zhang
      */
-    public Response<Api> registerApi(WebApplicationContext webApplicationContext, Api api) {
-
+    public Response<Api> registerApi_v1_5(Api api) {
         Method method = null;
-
         // 从handler中获取方法，获取不到就失败
         try {
             method = ApiController.class.getMethod("requestApi");
         } catch (NoSuchMethodException e) {
             e.printStackTrace();
-            return ResponseCreater.createFailResponse();
+            return ResponseCreator.createFailResponse();
         }
 
-        int index = ApiCache.addApi(api);
-
-        // 如果是替换了api则首先要注销原有的api，不然就会出错
-        if (index != -1) {
-            justUnRegisterApi(webApplicationContext, api);
+        if (ApiCache_v1_5.containsApi(api)) {
+            return ResponseCreator.createFailResponse("该Api已经存在");
         }
 
-        // 设置需要响应的内容
-        // ApiController.setResponseBody(api.getResponseBody());
+        ApiCache_v1_5.putApi(api);
 
         // 获取注解方式使用的 处理器映射器
-        RequestMappingHandlerMapping handlerMapping = webApplicationContext.getBean(RequestMappingHandlerMapping.class);
+        RequestMappingHandlerMapping handlerMapping = webApplicationAware.getWebApplicationContext().getBean(RequestMappingHandlerMapping.class);
 
         RequestMappingInfo mappingInfo = buildRequestMappingInfo(api);
         // 注册处理器
         handlerMapping.registerMapping(mappingInfo, "apiController", method);
 
-        return ResponseCreater.createSuccessResponse(api);
+        return ResponseCreator.createSuccessResponse(api, "注册成功");
+
     }
 
+    /***
+     * 注册Api如果存在相同的api则覆盖
+     */
+    @Override
+    public Map<String, Object> registerApiRe(Api api) {
+        Map<String,Object> res = new HashMap<>();
+        Method method = null;
+        // 从handler中获取方法，获取不到就失败
+        try {
+            method = ApiController.class.getMethod("requestApi");
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+            res.put("status",Response.FAIL);
+            res.put("msg","注册失败");
+            return res;
+        }
+
+        // 如果原先存在相同的Api，把被覆盖标志改成true，反之，改成false
+        // 如果原先存在相同的Api，把原先的处理器删除
+        if (ApiCache_v1_5.containsApi(api)) {
+            justUnRegisterApi(api);
+            res.put("isCover","true");
+        }
+        else {
+            res.put("isCover","false");
+        }
+
+        ApiCache_v1_5.putApi(api);
+
+        // 获取注解方式使用的 处理器映射器
+        RequestMappingHandlerMapping handlerMapping = webApplicationAware.getWebApplicationContext().getBean(RequestMappingHandlerMapping.class);
+
+        RequestMappingInfo mappingInfo = buildRequestMappingInfo(api);
+        // 注册处理器
+        handlerMapping.registerMapping(mappingInfo, "apiController", method);
+
+        res.put("msg","注册成功");
+        res.put("responseBody",api);
+        res.put("status",0);
+
+        return res;
+    }
 
     /***
      * 获取所有的api
      */
-    public Response<List<Api>> getAllApi() {
-        return ResponseCreater.createSuccessResponse(ApiCache.getAllApi());
+    public Response<Set<Api>> getAllApi() {
+        Set<Api> apis = ApiCache_v1_5.getAllApi();
+        return ResponseCreator.createSuccessResponse(apis);
     }
 
     /***
-     * 注销api的同时，将其从api缓存中删除
+     * 根据uuid删除api
      */
     @Override
-    public Response unRegisterApi(WebApplicationContext webApplicationContext, Api api, int index) {
-        justUnRegisterApi(webApplicationContext, api);
-        ApiCache.removeApi(index);
-        return ResponseCreater.createSuccessResponse();
+    public Response unRegisterApiById(String uuid) {
+        if (!ApiCache_v1_5.containsApi(uuid)) {
+            return ResponseCreator.createFailResponse("不存在这个Api");
+        }
+        Api api = ApiCache_v1_5.removeApi(uuid);
+        if (api == null) {
+            return ResponseCreator.createFailResponse("删除Api失败");
+        }
+        justUnRegisterApi(api);
+        return ResponseCreator.createSuccessResponse("注销Api成功");
     }
 
     /***
-    * 根据uuid删除api
-    */
-    @Override
-    public Response unRegisterApiById(WebApplicationContext webApplicationContext, String uuid) {
-        Api api = ApiCache.deleteApiByUUID(uuid);
-        if(api == null){
-            return ResponseCreater.createFailResponse();
+     * 删除所有API
+     */
+    public Response unRegisterAllApi() {
+        Set<Api> apis = ApiCache_v1_5.getAllApi();
+        for (Api api : apis) {
+            justUnRegisterApi(api);
+            ApiCache_v1_5.removeApi(api.getId());
         }
-        justUnRegisterApi(webApplicationContext,api);
-        return ResponseCreater.createSuccessResponse();
-    }
-
-    /***
-    * 删除所有API
-    */
-    public Response unRegisterAllApi(WebApplicationContext webApplicationContext){
-        List<Api> apis = ApiCache.getAllApi();
-        for(Api a : apis){
-            justUnRegisterApi(webApplicationContext, a);
-        }
-        ApiCache.removeAll();
-        return ResponseCreater.createSuccessResponse();
+        // 注意:返回的信息中会存在所有被删除的api信息
+        return ResponseCreator.createSuccessResponse(apis,"删除成功");
     }
 
     /***
@@ -129,11 +169,11 @@ public class PortalService implements IPortalService {
     }
 
     /***
-     * 只注销api
+     * 注销处理器
      */
-    private void justUnRegisterApi(WebApplicationContext webApplicationContext, Api api) {
+    private void justUnRegisterApi(Api api) {
         // 获取注解的方式使用的 处理器映射器
-        RequestMappingHandlerMapping handlerMapping = webApplicationContext.getBean(RequestMappingHandlerMapping.class);
+        RequestMappingHandlerMapping handlerMapping = webApplicationAware.getWebApplicationContext().getBean(RequestMappingHandlerMapping.class);
 
         RequestMappingInfo mappingInfo = buildRequestMappingInfo(api);
 
